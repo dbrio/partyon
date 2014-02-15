008d0736e663efe9ee5e4ba47e590a43c5cb0a17
dbrio2011 <dbrio2011@gmail.com>
2014-02-15 15:46:25 +0000

    actualizacion para las imagenes

---
+++ PartyOn/model/modelActivity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyOn.model
+{
+   public class modelActivity
+    {
+       public string UserFirstName { get; set; }
+    }
+}
