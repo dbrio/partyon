0c7ccece07f4fe3b5be30493ce139adefbdc26f8
jdoavila@gmail.com <jdoavila@gmail.com@6a433e61-3067-58a5-0fa3-3886736a1401>
2014-03-18 15:37:14 +0000

    ya hace el post de new place en web.

--- trunk/PartyOn/addPost.xaml.cs
+++ trunk/PartyOn/addPost.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using Microsoft.Phone.Tasks;
 using System.IO.IsolatedStorage;
 using System.Windows.Media.Imaging;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace PartyOn
 {
@@ -29,12 +34,91 @@ namespace PartyOn
             camera.Completed += new EventHandler<PhotoResult>(camera_Complete);
 
         }
+        private static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        private void UploadData()
+        {
+            string webpageContent = string.Empty;
+
+
+            Uri url = new Uri("http://192.168.2.8:8000/API/APIsaveplace/", UriKind.RelativeOrAbsolute);
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://192.168.2.8:8000/API/APIsaveplace/");
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Method = "POST";
+            //webRequest.ContentLength = byteArray.Length;
+
+            webRequest.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), webRequest);
+
+            //allDone.WaitOne();
+            
+
+            //WebClient webClient = new WebClient();
+            //webClient.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            //var uri = new Uri("http://127.0.0.1:8000/API/APIsaveplace", UriKind.Absolute);
+            //StringBuilder postData = new StringBuilder();
+            //postData.AppendFormat("{0}={1}", "PlaceName", HttpUtility.UrlEncode("Manguito2 desde WP8e"));
+            //postData.AppendFormat("&{0}={1}", "PlaceLat", HttpUtility.UrlEncode("27373.212"));
+            //postData.AppendFormat("&{0}={1}", "PlaceLong", HttpUtility.UrlEncode("87783312.12"));
+
+            //webClient.Headers[HttpRequestHeader.ContentLength] = postData.Length.ToString();
+            //webClient.UploadStringCompleted += new UploadStringCompletedEventHandler(webClient_UploadStringCompleted);
+            //webClient.UploadProgressChanged += webClient_UploadProgressChanged;
+            //webClient.UploadStringAsync(uri, "POST", postData.ToString());
+        }
+
+        private static void GetRequestStreamCallback(IAsyncResult asynchronousResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
+
+            Stream postStream = request.EndGetRequestStream(asynchronousResult);
+
+            string postData = "PlaceName=ManguitosWP82&PlaceLat=239231222&PlaceLong=98856578441";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            postStream.Write(byteArray, 0, postData.Length);
+            postStream.Close();
+
+            request.BeginGetResponse(new AsyncCallback(GetResponseCallback), request);
+
+        }
+
+
+        private static void GetResponseCallback(IAsyncResult asynchronousResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
+
+            HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
+            Stream streamResponse = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(streamResponse);
+            string responseString = streamReader.ReadToEnd();
+            Console.WriteLine(responseString);
+            Deployment.Current.Dispatcher.BeginInvoke(() => MessageBox.Show(responseString, "Respuesta del servidor web.", MessageBoxButton.OK));
+            streamResponse.Close();
+            streamReader.Close();
+
+            response.Close();
+            //allDone.Set();
+        }
+
+        //void webClient_UploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+        //{
+        //    Debug.WriteLine(string.Format("Progress: {0}", e.ProgressPercentage));
+        //}
+
+        //private void webClient_UploadStringCompleted(object sender, UploadStringCompletedEventArgs e)
+        //{
+        //    Debug.WriteLine("Completed.");
+        //    MessageBox.Show("Completado.");
+        //}
+
 
         private void camera_Complete(object sender, PhotoResult e)
         {
             if(e.TaskResult==TaskResult.OK)
             {
-                GuardarEnAlmacenamientoAislado(e.ChosenPhoto);
+                UploadData();
+                //GuardarEnAlmacenamientoAislado(e.ChosenPhoto);
             }
         }
 
