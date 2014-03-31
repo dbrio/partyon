0bd8ce5555dbf2e1e02aaa0cc523517b305534ad
jdoavila@gmail.com <jdoavila@gmail.com@6a433e61-3067-58a5-0fa3-3886736a1401>
2014-03-31 00:17:19 +0000

    Login funciona correctamente.

---
+++ trunk/PartyOn/model/userData/userDataDbDataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace PartyOn.model.userData
+{
+    class userDataDbDataContext : DataContext
+    {
+        public userDataDbDataContext(string conectionString)
+            : base(conectionString)
+        {
+
+        }
+
+        public Table<userDataDB> userDataDB2
+        {
+            get
+            {
+                return this.GetTable<userDataDB>();
+            }
+        }
+    }
+}
