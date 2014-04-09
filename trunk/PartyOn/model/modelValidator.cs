74cc34d83cbc6a23c3b5a31bd4d1f045634122c1
jdoavila@gmail.com <jdoavila@gmail.com@6a433e61-3067-58a5-0fa3-3886736a1401>
2014-04-09 00:39:43 +0000

    Ya va quedando esto primo....

---
+++ trunk/PartyOn/model/modelValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyOn.model
+{
+    public class modelValidator
+    {
+        public string Available { get; set; }
+        public string Error { get; set; }
+    }
+}
