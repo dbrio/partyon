c8151b0b195689e44afb987640b9b166b2dca329
dbrio2011@gmail.com <dbrio2011@gmail.com@6a433e61-3067-58a5-0fa3-3886736a1401>
2014-04-12 20:24:37 +0000

    mejoras de dise;o

--- trunk/PartyOn/login.xaml.cs
+++ trunk/PartyOn/login.xaml.cs
@@ -73,7 +73,7 @@ namespace PartyOn
 
         private void HacerLogin()
         {
-            Uri url = new Uri(string.Format("http://www.partyonapp.com/API/login/?username={0}&password={1}", txtUser.Text, txtPassword.Password), UriKind.Absolute);
+            Uri url = new Uri(string.Format("http://www.partyonapp.com/API/login/?username={0}&password={1}", txtUser.Text, txtPassword.Text), UriKind.Absolute);
             WebClient ClienteWeb = new WebClient();
             ClienteWeb.DownloadStringCompleted += ClienteWeb_DownloadStringCompleted;
             ClienteWeb.DownloadStringAsync(url);
@@ -96,7 +96,7 @@ namespace PartyOn
             else if (NavigationContext.QueryString.ContainsKey("username") && NavigationContext.QueryString.ContainsKey("pass"))
             {
                 txtUser.Text = Convert.ToString(NavigationContext.QueryString["username"]);
-                txtPassword.Password = Convert.ToString(NavigationContext.QueryString["pass"]);
+                txtPassword.Text = Convert.ToString(NavigationContext.QueryString["pass"]);
                 NavigationService.RemoveBackEntry();
 
                 pbLogin.Visibility = System.Windows.Visibility.Visible;
@@ -147,7 +147,7 @@ namespace PartyOn
                     else
                     {
                         MessageBox.Show("Username or password incorrect, please try again.", "PartyOn", MessageBoxButton.OK);
-                        txtPassword.Password = "";
+                        txtPassword.Text = "";
                         btnEntrar.IsEnabled = true;
                     }
                 }
@@ -164,7 +164,7 @@ namespace PartyOn
             else
             {
                 MessageBox.Show("Username or password incorrect, please try again.", "PartyOn", MessageBoxButton.OK);
-                txtPassword.Password = "";
+                txtPassword.Text = "";
                 pbLogin.Visibility = System.Windows.Visibility.Collapsed;
                 btnEntrar.IsEnabled = true;
             }
@@ -188,6 +188,10 @@ namespace PartyOn
             webTerms.Uri = new Uri("http://www.partyonapp.com/user/password/reset/sendmail/", UriKind.Absolute);
             webTerms.Show();
         }
+
+       
+
+      
 
     }
 }
